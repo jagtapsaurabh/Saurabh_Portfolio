8c048ec72867e2b3883014ff2a85d78955cbd755
68534_Saurabhj <jagtapsaurabh98@gmail.com>
2023-12-10 13:31:25 +0530

    commit changes

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -24,7 +24,7 @@ function Projects() {
               isBlog={false}
               title="E-Voting System"
               description="The Fingerprint-Based E-Voting System is a cutting-edge digital solution that merges the convenience of electronic voting with the robustness of biometric authentication. This innovative system aims to revolutionize the traditional voting process by introducing a secure and efficient method of casting votes using fingerprint identification."
-              ghLink="https://github.com/abhijeetbhosale11/E-Voting-System"
+              ghLink="https://github.com/jagtapsaurabh/E-Voting-System"
               demoLink=" "
             />
           </Col>
@@ -33,9 +33,9 @@ function Projects() {
             <ProjectCard
               imgPath={bitsOfCode}
               isBlog={false}
-              title="3D Visuals Portfolio"
+              title=" Portfolio"
               description="Embark on a visual journey through the realm of cutting-edge web development with Unveiling Dimensions, a dynamic 3D developer portfolio crafted using React.js and Three.js. Immerse yourself in a captivating showcase that seamlessly blends the artistry of web design with the power of interactive 3D graphics."
-              ghLink="https://github.com/abhijeetbhosale11/3D-Portfolio"
+              ghLink="https://github.com/jagtapsaurabh/Saurabh_Portfolio"
               demoLink=" "
             />
           </Col>
@@ -46,7 +46,7 @@ function Projects() {
               isBlog={false}
               title="Flight Booking System"
               description="The Flight Booking System is a sophisticated and user-friendly software solution designed to streamline and enhance the entire flight booking process for travelers and airlines alike. This innovative system represents a significant leap forward in the world of travel technology, offering a range of features and benefits that ensure a seamless."
-              ghLink="https://github.com/abhijeetbhosale11/Flight-Booking-System"
+              ghLink="https://github.com/jagtapsaurabh/Flight-Booking-System"
               demoLink=" "              
             />
           </Col>
